c8c2c289cdc7f60c1940d76890924a39d2daaba3
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-04-13 08:35:01 -0400

    Updated from Brython Server: 4/13/2018 8:35:01 AM

--- listDemo.py
+++ listDemo.py
@@ -6,3 +6,8 @@ words = input('Enter some words: ').split(' ')
 
 print('The first word was: ', words[0])
 print('The last word was: ', words[-1])
+
+
+#print out list one at a time
+for time in words:
+    print(item)
