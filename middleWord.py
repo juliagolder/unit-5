b525a31b8b9e43f3078a986749fc9ff2ce5140ef
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-04-23 08:40:53 -0400

    Updated from Brython Server: 4/23/2018 8:40:53 AM

--- middleWord.py
+++ middleWord.py
@@ -6,4 +6,10 @@ word = input('Enter some words: ').split(' ')
 
 total = len(word)
 
-print(word[total/2])
+
+
+if total%2 != 0:
+    print(word[total/2])
+else:
+    print(word[total/2-1])
+    print(word[total/2])
