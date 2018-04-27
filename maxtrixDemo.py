9cb3defb3b3ffed62aca7779e7f3ff2364782168
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-04-27 08:30:10 -0400

    Updated from Brython Server: 4/27/2018 8:30:11 AM

--- maxtrixDemo.py
+++ maxtrixDemo.py
@@ -4,7 +4,9 @@
 
 def printBoard(board):
     for r in range(0,3):
-        print(board[r])
+        for c in range(0,3):
+            print(board[r][c],end=' ')
+        print()
 
 board = [['a','b','c'],['d','e','f'],['g','h','i']]
 printBoard(board)
