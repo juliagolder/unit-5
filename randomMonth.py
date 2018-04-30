2c0d567e9af10cf80323b53a08491dc18ab8bb72
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-04-30 08:48:33 -0400

    Updated from Brython Server: 4/30/2018 8:48:33 AM

--- randomMonth.py
+++ randomMonth.py
@@ -1,1 +1,20 @@
+#juliagolder
+#4/30/18
+#randomMoth.py
 
+from random import randint
+
+1 = 'January'
+2 = 'Febuary'
+3 = 'March'
+4 = 'April'
+5 = 'May'
+6 = 'June'
+7 = 'July'
+8 = 'August'
+9 = 'September'
+10 = 'October'
+11 = 'November'
+12 = 'December'
+
+print(randint(1,12))
