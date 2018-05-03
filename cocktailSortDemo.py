d1baffa3b3547238944d079c70dde40ebe4b1ab3
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-05-03 08:15:00 -0400

    Updated from Brython Server: 5/3/2018 8:15:02 AM

--- cocktailSortDemo.py
+++ cocktailSortDemo.py
@@ -39,7 +39,6 @@ if __name__ == '__main__':
     #numbers = mySort(numbers)
     numbers = numbers.sort()
     t2 = time()
-    
     #print whether the sort worked or not
     try:
         assert(numbers == pythonSort)
