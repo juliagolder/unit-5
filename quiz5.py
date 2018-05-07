366fb613fda0dfa567661007aea7548572d874f4
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-05-07 08:33:29 -0400

    Updated from Brython Server: 5/7/2018 8:33:30 AM

--- quiz5.py
+++ quiz5.py
@@ -2,6 +2,22 @@
 #5/7/18
 #quiz5.py
 
+"""
+def penultimate(x):
+    print(x[-2])
+
+print(penultimate([3,4,5,6,7]))
+"""
+
+def plusEquals
+
+   
+"""
+    for i in range(1,x+1):
+        print(i)
+    print('BOOM!')
+
+print(word.upper() + '!!!')
 
 def 
 for w in penultimate:
@@ -16,6 +32,8 @@ def plusEquals(L):
     return new
 
 print(plusEquals([1,2,3,4],10))
+"""
+
 
 """
 def plusEquals(j)
