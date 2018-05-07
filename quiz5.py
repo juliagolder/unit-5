7278daa96fb45637e5598a9814535728636f843a
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-05-07 08:50:23 -0400

    Updated from Brython Server: 5/7/2018 8:50:24 AM

--- quiz5.py
+++ quiz5.py
@@ -2,55 +2,23 @@
 #5/7/18
 #quiz5.py
 
-"""
+
 def penultimate(x):
     print(x[-2])
 
 print(penultimate([3,4,5,6,7]))
-"""
 
-def plusEquals
-
-   
-"""
-    for i in range(1,x+1):
-        print(i)
-    print('BOOM!')
-
-print(word.upper() + '!!!')
-
-def 
-for w in penultimate:
-        print(w[-2])
-        
-print(penultimate([3,4,5,6,7]))
 
 def plusEquals(L):
     new = []
     for item in L:
-        new.append(item+x)
+        new.append(item+10)
     return new
 
-print(plusEquals([1,2,3,4],10))
-"""
+print(plusEquals([1,2,3]))
 
 
-"""
-def plusEquals(j)
+def smallest(T):
+    print(len(number))
+print(smallest([1,2,3,4]))
 
-
-def smallest(s):
-
-word = input('Enter some words: ').split(' ') #splits words into list
-
-longest = 0
-LONGword = '' #label the variable
-
-
-for item in word: #loop to find the longest word
-    if longest < (len(item)):
-        longest = (len(item))
-        LONGword = item
-
-print(LONGword) #prints longest word
-"""
